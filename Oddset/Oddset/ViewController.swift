b4cc3cb1d37ccf104f5ba4b03f18c1a061ce76a4
Frederik Nagels <nagels97@live.dk>
2018-05-30 19:02:32 +0200

    First start

---
+++ Oddset/Oddset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oddset
+//
+//  Created by Full On Pimp on 30/05/2018.
+//  Copyright © 2018 nagels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
